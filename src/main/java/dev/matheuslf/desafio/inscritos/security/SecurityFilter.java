a9b832e1b19e1d6c4bb2749bb138d75a765410ea
Marcos Allan <marcosallanm1to@gmail.com>
2025-11-22 14:54:32 -0300

    feat: tratando verificação de usuário enviado no token

--- src/main/java/dev/matheuslf/desafio/inscritos/security/SecurityFilter.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/security/SecurityFilter.java
@@ -1,8 +1,5 @@
 package dev.matheuslf.desafio.inscritos.security;
 
-import dev.matheuslf.desafio.inscritos.entities.User;
-import dev.matheuslf.desafio.inscritos.exception.ResourceNotFoundException;
-import dev.matheuslf.desafio.inscritos.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,27 +18,31 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
-    private final UserRepository userRepository;
+    private final CustomUserDetailsService customUserDetailsService;
     private final TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = recoverToken(request);
+        try {
+            String token = recoverToken(request);
 
-        if (token != null) {
-            String email = tokenService.validateToken(token);
+            if (token != null) {
+                String email = tokenService.validateToken(token);
 
-            if (email != null) {
-                User user = userRepository.findByEmail(email)
-                        .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+                if (email != null) {
+                    UserDetails user = customUserDetailsService.loadUserByUsername(email);
 
-                Authentication authentication = new UsernamePasswordAuthenticationToken(
-                        user,
-                        null,
-                        user.getAuthorities()
-                );
-                SecurityContextHolder.getContext().setAuthentication(authentication);}
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            user.getAuthorities()
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        } catch (Exception ignored) {
         }
+
         filterChain.doFilter(request, response);
     }
 
